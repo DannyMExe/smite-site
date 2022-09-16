eaea1ff27d1d0b81b6ca017239b7b79e074fabeb
Danny Laptop <dannyjmargolin@gmail.com>
2022-09-16 18:05:59 -0400

    dontenv webpack

--- seedInfo/users.js
+++ seedInfo/users.js
@@ -1,15 +1,17 @@
-const users = [{
-    username: "thgexecutor",
-    email: "dannyjmargolin@gmail.com",
-    password: "12345678",
-},{
-    username: "weak3n",
-    email: "kurt@gmail.com",
-    password: "87654321",
-},{
-    username: "supercraz195",
-    email: "jdolly@gmail.com",
-    password: "sacpase123",
-}];
+const users = [
+//     {
+//     username: "thgexecutor",
+//     email: "dannyjmargolin@gmail.com",
+//     password: "12345678",
+// },{
+//     username: "weak3n",
+//     email: "kurt@gmail.com",
+//     password: "87654321",
+// },{
+//     username: "supercraz195",
+//     email: "jdolly@gmail.com",
+//     password: "sacpase123",
+// }
+];
 
 module.exports = users;
