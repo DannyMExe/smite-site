71dccd81b88b54ea4adc31a8500da14353fea000
Danny Laptop <dannyjmargolin@gmail.com>
2022-09-16 18:03:07 -0400

    dontenv webpack

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { BrowserRouter } from 'react-router-dom';
 import store from './store/index'
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import { AuthProvider } from '../src/contexts/AuthContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
