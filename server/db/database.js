15b8ccccc9410ac440ae1a709b02fc7a95e74f27
Danny Laptop <dannyjmargolin@gmail.com>
2022-09-16 17:28:55 -0400

    dontenv webpack

--- server/db/database.js
+++ server/db/database.js
@@ -1,7 +1,14 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize(
     process.env.DATABASE_URL ||
-    'postgres://localhost:5432/smite-site', { logging: false }
+    'postgres://localhost:5432/smite-site', {
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
+        }
+      }
     );
 
 module.exports = db;
