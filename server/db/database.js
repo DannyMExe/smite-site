e81e7dc71137243382e32e4c98918e3989c5d034
Danny Laptop <dannyjmargolin@gmail.com>
2022-09-16 17:13:08 -0400

    db url

--- server/db/database.js
+++ server/db/database.js
@@ -1,5 +1,8 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/smite-site', { logging: false });
+const db = new Sequelize(
+    process.env.DATABASE_URL ||
+    'postgres://localhost:5432/smite-site', { logging: false }
+    );
 
 module.exports = db;
 
